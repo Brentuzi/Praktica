460c78fabbaeb03b4453c862d051d3f371c98781
Brentuzi <90348225+Brentuzi@users.noreply.github.com>
2023-03-28 11:08:12 +0300

    labirint work

--- PAD_Task_5/Program.cs
+++ PAD_Task_5/Program.cs
@@ -1,185 +1,240 @@
 ﻿using System;
-
 using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Collections.Generic;
+
 namespace PAD_Task_5
 {
     class Program
     {
-        static void Main(string[] args)
+        private static char[,] map;
+        private static int playerX, playerY;
+        private static int[] enemyPositions;
+        private static Random random = new Random();
+        private static int playerHealth = 100;
+
+        private static void LoadMap(string fileName)
         {
-            // Загрузка карты из файла
-            string[] lines = File.ReadAllLines("map.txt");
-            int rows = lines.Length;
-            int cols = lines[0].Length;
-            char[,] maze = new char[rows, cols];
-            for (int i = 0; i < rows; i++)
+            var lines = File.ReadAllLines(fileName);
+            int height = lines.Length;
+            int width = lines.Max(l => l.Length);
+
+            map = new char[height, width];
+
+            for (int i = 0; i < height; i++)
             {
-                for (int j = 0; j < cols; j++)
+                for (int j = 0; j < width; j++)
                 {
-                    maze[i, j] = lines[i][j];
+                    map[i, j] = lines[i].Length > j ? lines[i][j] : ' ';
+                }
+            }
+        }
+
+        private static void DrawMap()
+        {
+            Console.Clear();
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool CanMove(int x, int y)
+        {
+            return map[x, y] != '#';
+        }
+
+        private static bool CheckCollisionWithEnemy(int x, int y)
+        {
+            for (int i = 0; i < enemyPositions.Length; i += 2)
+            {
+                if (enemyPositions[i] == x && enemyPositions[i + 1] == y)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static void MovePlayer(int dx, int dy)
+        {
+            int newX = playerX + dx;
+            int newY = playerY + dy;
+            if (CanMove(newX, newY))
+            {
+                if (CheckCollisionWithEnemy(newX, newY))
+                {
+                    playerHealth -= 10;
+                }
+                else
+                {
+                    map[playerX, playerY] = ' ';
+                    playerX = newX;
+                    playerY = newY;
+                }
+                map[playerX, playerY] = '@';
+            }
+        }
+
+        private static void MoveEnemy()
+        {
+            for (int i = 0; i < enemyPositions.Length; i += 2)
+            {
+                int dx = random.Next(-1, 2);
+                int dy = random.Next(-1, 2);
+                int newX = enemyPositions[i] + dx;
+                int newY = enemyPositions[i + 1] + dy;
+
+                if (CanMove(newX, newY))
+                {
+                    map[enemyPositions[i], enemyPositions[i + 1]] = ' ';
+                    enemyPositions[i] = newX;
+                    enemyPositions[i + 1] = newY;
+                    map[newX, newY] = 'E';
+                }
+            }
+        }
+
+        private static void InitializeGame(string fileName)
+        {
+            LoadMap(fileName);
+            playerX = Array.FindIndex(map.Cast<char>().ToArray(), c => c == '@') / map.GetLength(1);
+            playerY = Array.FindIndex(map.Cast<char>().ToArray(), c => c == '@') % map.GetLength(1);
+
+            enemyPositions = new int[2 * map.Cast<char>().Count(c => c == 'E')];
+            int enemyIndex = 0;
+
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[i, j] == 'E')
+                    {
+                        enemyPositions[enemyIndex] = i;
+                        enemyPositions[enemyIndex + 1] = j;
+                        enemyIndex += 2;
+                    }
+                }
+            }
+        }
+        private static void DrawPlayerStatus()
+        {
+            Console.SetCursorPosition(0, map.GetLength(0));
+            Console.WriteLine($"Health: {playerHealth}%");
+            Console.WriteLine("управление W S D A P-показать путь");
+        }
+
+        private static void ShowPath()
+        {
+            int[] dx = { -1, 0, 1, 0 };
+            int[] dy = { 0, 1, 0, -1 };
+            int width = map.GetLength(1);
+            int height = map.GetLength(0);
+            int[,] dist = new int[height, width];
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    dist[i, j] = -1;
                 }
             }
 
-            // Нахождение начальной и конечной точек
-            int startX = 0;
-            int startY = 0;
-            int endX = 0;
-            int endY = 0;
-            for (int i = 0; i < rows; i++)
+            dist[playerX, playerY] = 0;
+            Queue<Tuple<int, int>> queue = new Queue<Tuple<int, int>>();
+            queue.Enqueue(new Tuple<int, int>(playerX, playerY));
+
+            while (queue.Count > 0)
             {
-                for (int j = 0; j < cols; j++)
+                Tuple<int, int> current = queue.Dequeue();
+                int x = current.Item1;
+                int y = current.Item2;
+
+                for (int i = 0; i < 4; i++)
                 {
-                    if (maze[i, j] == '@')
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+
+                    if (nx >= 0 && ny >= 0 && nx < height && ny < width && dist[nx, ny] == -1 && map[nx, ny] != '#')
                     {
-                        startX = i;
-                        startY = j;
-                    }
-                    else if (maze[i, j] == 'X')
-                    {
-                        endX = i;
-                        endY = j;
+                        dist[nx, ny] = dist[x, y] + 1;
+                        queue.Enqueue(new Tuple<int, int>(nx, ny));
                     }
                 }
             }
 
-            // Начальное положение игрока
-            int playerX = startX;
-            int playerY = startY;
+            int targetX = Array.FindIndex(map.Cast<char>().ToArray(), c => c == 'X') / map.GetLength(1);
+            int targetY = Array.FindIndex(map.Cast<char>().ToArray(), c => c == 'X') % map.GetLength(1);
 
-            // Основной игровой цикл
+
+            while (dist[targetX, targetY] > 1)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    int nx = targetX + dx[i];
+                    int ny = targetY + dy[i];
+
+                    if (nx >= 0 && ny >= 0 && nx < height && ny < width && dist[nx, ny] == dist[targetX, targetY] - 1)
+                    {
+                        map[targetX, targetY] = '.';
+                        targetX = nx;
+                        targetY = ny;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            InitializeGame("map.txt");
+           
             while (true)
             {
-                Console.Clear();
-                DrawMaze(maze);
-                Console.WriteLine("Use arrow keys to move, P to show path, Q to quit");
-                Console.WriteLine();
-
-                // Отображение состояния игрока
-                int health = 100;
-                Console.Write("Health: ");
-                DrawBar(health);
-
-                // Обработка ввода пользователя
+                DrawMap();
+                DrawPlayerStatus();
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-                if (keyInfo.Key == ConsoleKey.UpArrow && playerX > 0 && maze[playerX - 1, playerY] != '#')
+
+                int dx = 0, dy = 0;
+                switch (keyInfo.Key)
                 {
-                    playerX--;
+                    case ConsoleKey.A:
+                        dy = -1;
+                        break;
+                    case ConsoleKey.W:
+                        dx = -1;
+                        break;
+                    case ConsoleKey.D:
+                        dy = 1;
+                        break;
+                    case ConsoleKey.S:
+                        dx = 1;
+                        break;
+                    case ConsoleKey.P:
+                        ShowPath();
+                        break;
                 }
-                else if (keyInfo.Key == ConsoleKey.DownArrow && playerX < rows - 1 && maze[playerX + 1, playerY] != '#')
+
+                MovePlayer(dx, dy);
+                MoveEnemy();
+
+                if (playerHealth <= 0)
                 {
-                    playerX++;
-                }
-                else if (keyInfo.Key == ConsoleKey.LeftArrow && playerY > 0 && maze[playerX, playerY - 1] != '#')
-                {
-                    playerY--;
-                }
-                else if (keyInfo.Key == ConsoleKey.RightArrow && playerY < cols - 1 && maze[playerX, playerY + 1] != '#')
-                {
-                    playerY++;
-                }
-                else if (keyInfo.Key == ConsoleKey.P)
-                {
-                    ShowPath(maze, playerX, playerY, endX, endY);
-                    continue;
-                }
-                else if (keyInfo.Key == ConsoleKey.Q)
-                {
-                    Console.WriteLine("Выход...");
+                    Console.Clear();
+                    Console.WriteLine("Game Over");
                     break;
                 }
 
-                // Обновление карты
-                maze[startX, startY] = ' ';
-                maze[playerX, playerY] = '@';
-
-                // Проверка на победу
-                if (playerX == endX && playerY == endY)
-                {
-                    Console.WriteLine("Вы достигли финиша! Поздравляем!");
-                    break;
-                }
+                Thread.Sleep(50); // Задержка
             }
-        }
-
-        static void DrawMaze(char[,] maze)
-        {
-            // Рисование карты
-            for (int i = 0; i < maze.GetLength(0); i++)
-            {
-                for (int j = 0; j < maze.GetLength(1); j++)
-                {
-                    Console.Write(maze[i, j]);
-                }
-                Console.WriteLine();
-            }
-        }
-        static void DrawBar(int percent)
-        {
-            Console.Write("[");
-            int bars = percent / 10;
-            for (int i = 0; i < 10; i++)
-            {
-                if (i < bars)
-                {
-                    Console.Write("#");
-                }
-                else
-                {
-                    Console.Write("_");
-                }
-            }
-            Console.Write("]");
-        }
-
-        static char[,] FindPath(char[,] maze, int startX, int startY, int endX, int endY)
-        {
-            // Копирование карты, чтобы не изменять исходную карту
-            char[,] copy = (char[,])maze.Clone();
-
-            // Настройка начальной и конечной точек
-            copy[startX, startY] = ' ';
-            copy[endX, endY] = 'X';
-
-            // Нахождение пути с помощью алгоритма "левая рука"
-            int x = startX;
-            int y = startY + 1;
-            int dx = 0;
-            int dy = 1;
-            while (x != endX || y != endY)
-            {
-                if (copy[x + dx, y + dy] == ' ')
-                {
-                    x += dx;
-                    y += dy;
-                }
-                else if (copy[x - dy, y + dx] == ' ')
-                {
-                    int temp = dx;
-                    dx = -dy;
-                    dy = temp;
-                    x -= dy;
-                    y += dx;
-                }
-                else
-                {
-                    int temp = dx;
-                    dx = dy;
-                    dy = -temp;
-                    x += dy;
-                    y -= dx;
-                }
-                copy[x, y] = '+';
-            }
-
-            return copy;
-        }
-
-        static void ShowPath(char[,] maze, int startX, int startY, int endX, int endY)
-        {
-            char[,] path = FindPath(maze, startX, startY, endX, endY);
-            DrawMaze(path);
         }
     }
+
 
 
 }
